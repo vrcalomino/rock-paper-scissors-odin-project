6ccf35e9d81b5f3ed53a48564976d4cc836e6619
vrcalomino <vrcalomino@gmail.com>
2023-08-12 20:36:14 -0300

    get random selection function

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-var message = "Hello world";
-console.log(message);
+var getComputerChoice = function () {
+    var options = ["Rock", "Paper", "Scissors"];
+    var randomNumber = Math.floor(Math.random() * 3);
+    return options[randomNumber];
+};
+console.log(getComputerChoice());
--- index.ts
+++ index.ts
@@ -1,2 +1,5 @@
-const message: string = "Hello world";
-console.log(message);
+const getComputerChoice = (): string => {
+  const options = ["Rock", "Paper", "Scissors"];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return options[randomNumber];
+};
