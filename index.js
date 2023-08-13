41dd7dc964a188b5b0811d80cbbc596291131129
vrcalomino <vrcalomino@gmail.com>
2023-08-13 01:03:44 -0300

    play round function

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
+var ROCK = "rock";
+var PAPER = "paper";
+var SCISSORS = "scissors";
 var getComputerChoice = function () {
-    var options = ["Rock", "Paper", "Scissors"];
+    var options = [ROCK, PAPER, SCISSORS];
     var randomNumber = Math.floor(Math.random() * 3);
     return options[randomNumber];
 };
-console.log(getComputerChoice());
+var playRound = function (playerSelection, computerSelection) {
+    console.log(playerSelection, computerSelection);
+    if (playerSelection === ROCK && computerSelection === SCISSORS) {
+        return "You won! ".concat(ROCK.charAt(0).toUpperCase()).concat(ROCK.slice(1), " beats ").concat(SCISSORS.charAt(0).toUpperCase()).concat(SCISSORS.slice(1));
+    }
+    else if (playerSelection === ROCK && computerSelection === PAPER) {
+        return "You lose! ".concat(ROCK.charAt(0).toUpperCase()).concat(ROCK.slice(1), " loses to ").concat(PAPER.charAt(0).toUpperCase()).concat(PAPER.slice(1));
+    }
+    else if (playerSelection === PAPER && computerSelection === ROCK) {
+        return "You won! ".concat(PAPER.charAt(0).toUpperCase()).concat(PAPER.slice(1), " beats ").concat(ROCK.charAt(0).toUpperCase()).concat(ROCK.slice(1));
+    }
+    else if (playerSelection === PAPER && computerSelection === SCISSORS) {
+        return "You lose! ".concat(PAPER.charAt(0).toUpperCase()).concat(PAPER.slice(1), " loses to ").concat(SCISSORS.charAt(0).toUpperCase()).concat(SCISSORS.slice(1));
+    }
+    else if (playerSelection === SCISSORS && computerSelection === PAPER) {
+        return "You won! ".concat(SCISSORS.charAt(0).toUpperCase()).concat(SCISSORS.slice(1), " beats ").concat(PAPER.charAt(0).toUpperCase()).concat(PAPER.slice(1));
+    }
+    else if (playerSelection === SCISSORS && computerSelection === ROCK) {
+        return "You lose! ".concat(SCISSORS.charAt(0).toUpperCase()).concat(SCISSORS.slice(1), " loses to ").concat(ROCK.charAt(0).toUpperCase()).concat(ROCK.slice(1));
+    }
+    else {
+        return "It's a tie!";
+    }
+};
+var comp = getComputerChoice();
+console.log(playRound(ROCK, comp));
--- index.ts
+++ index.ts
@@ -1,5 +1,46 @@
+const ROCK = "rock";
+const PAPER = "paper";
+const SCISSORS = "scissors";
+
 const getComputerChoice = (): string => {
-  const options = ["Rock", "Paper", "Scissors"];
+  const options = [ROCK, PAPER, SCISSORS];
   const randomNumber = Math.floor(Math.random() * 3);
   return options[randomNumber];
 };
+
+const playRound = (
+  playerSelection: string,
+  computerSelection: string
+): string => {
+  console.log(playerSelection, computerSelection);
+  if (playerSelection === ROCK && computerSelection === SCISSORS) {
+    return `You won! ${ROCK.charAt(0).toUpperCase()}${ROCK.slice(
+      1
+    )} beats ${SCISSORS.charAt(0).toUpperCase()}${SCISSORS.slice(1)}`;
+  } else if (playerSelection === ROCK && computerSelection === PAPER) {
+    return `You lose! ${ROCK.charAt(0).toUpperCase()}${ROCK.slice(
+      1
+    )} loses to ${PAPER.charAt(0).toUpperCase()}${PAPER.slice(1)}`;
+  } else if (playerSelection === PAPER && computerSelection === ROCK) {
+    return `You won! ${PAPER.charAt(0).toUpperCase()}${PAPER.slice(
+      1
+    )} beats ${ROCK.charAt(0).toUpperCase()}${ROCK.slice(1)}`;
+  } else if (playerSelection === PAPER && computerSelection === SCISSORS) {
+    return `You lose! ${PAPER.charAt(0).toUpperCase()}${PAPER.slice(
+      1
+    )} loses to ${SCISSORS.charAt(0).toUpperCase()}${SCISSORS.slice(1)}`;
+  } else if (playerSelection === SCISSORS && computerSelection === PAPER) {
+    return `You won! ${SCISSORS.charAt(0).toUpperCase()}${SCISSORS.slice(
+      1
+    )} beats ${PAPER.charAt(0).toUpperCase()}${PAPER.slice(1)}`;
+  } else if (playerSelection === SCISSORS && computerSelection === ROCK) {
+    return `You lose! ${SCISSORS.charAt(0).toUpperCase()}${SCISSORS.slice(
+      1
+    )} loses to ${ROCK.charAt(0).toUpperCase()}${ROCK.slice(1)}`;
+  } else {
+    return "It's a tie!";
+  }
+};
+
+const comp: string = getComputerChoice();
+console.log(playRound(ROCK, comp));
