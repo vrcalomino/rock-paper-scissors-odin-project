30eba5b67a339d20e95acc324f520013bb1d68bf
vrcalomino <vrcalomino@gmail.com>
2023-08-13 01:05:28 -0300

    correction to play round function, user input case insensitive

--- index.ts
+++ index.ts
@@ -13,27 +13,28 @@ const playRound = (
   computerSelection: string
 ): string => {
   console.log(playerSelection, computerSelection);
-  if (playerSelection === ROCK && computerSelection === SCISSORS) {
+  const playerSelectionFinal: string = playerSelection.toLowerCase();
+  if (playerSelectionFinal === ROCK && computerSelection === SCISSORS) {
     return `You won! ${ROCK.charAt(0).toUpperCase()}${ROCK.slice(
       1
     )} beats ${SCISSORS.charAt(0).toUpperCase()}${SCISSORS.slice(1)}`;
-  } else if (playerSelection === ROCK && computerSelection === PAPER) {
+  } else if (playerSelectionFinal === ROCK && computerSelection === PAPER) {
     return `You lose! ${ROCK.charAt(0).toUpperCase()}${ROCK.slice(
       1
     )} loses to ${PAPER.charAt(0).toUpperCase()}${PAPER.slice(1)}`;
-  } else if (playerSelection === PAPER && computerSelection === ROCK) {
+  } else if (playerSelectionFinal === PAPER && computerSelection === ROCK) {
     return `You won! ${PAPER.charAt(0).toUpperCase()}${PAPER.slice(
       1
     )} beats ${ROCK.charAt(0).toUpperCase()}${ROCK.slice(1)}`;
-  } else if (playerSelection === PAPER && computerSelection === SCISSORS) {
+  } else if (playerSelectionFinal === PAPER && computerSelection === SCISSORS) {
     return `You lose! ${PAPER.charAt(0).toUpperCase()}${PAPER.slice(
       1
     )} loses to ${SCISSORS.charAt(0).toUpperCase()}${SCISSORS.slice(1)}`;
-  } else if (playerSelection === SCISSORS && computerSelection === PAPER) {
+  } else if (playerSelectionFinal === SCISSORS && computerSelection === PAPER) {
     return `You won! ${SCISSORS.charAt(0).toUpperCase()}${SCISSORS.slice(
       1
     )} beats ${PAPER.charAt(0).toUpperCase()}${PAPER.slice(1)}`;
-  } else if (playerSelection === SCISSORS && computerSelection === ROCK) {
+  } else if (playerSelectionFinal === SCISSORS && computerSelection === ROCK) {
     return `You lose! ${SCISSORS.charAt(0).toUpperCase()}${SCISSORS.slice(
       1
     )} loses to ${ROCK.charAt(0).toUpperCase()}${ROCK.slice(1)}`;
